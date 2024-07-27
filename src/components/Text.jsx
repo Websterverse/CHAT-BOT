c33c92fae500245260fdbd0874ac195d7830ae90
KING <iamkingkarr@gmail.com>
2024-07-27 16:07:31 +0530

    UI UX modify

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react'
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import loading_img from "../assets/loading.gif"
+import loading_img from "../assets/l.gif"
 const Text = () => {
 
 // Access your API key as an environment variable.
@@ -42,13 +42,13 @@ copyref.current.select()
 
 
     return (
-        <div className='min-h-screen bg-gray-100 flex items-center justify-center  ' >
+        <div className='min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center  ' >
             <div className='max-w-3xl w-full md:w-2/3 lg:w-1/2 mx-4 my-8 p-6 bg-white rounded-lg shadow-lg  '  >
 
-                <h1 className='text-3xl font-bold  text-center mb-4 text-gray-500 '  >AI TEXT GENERATOR </h1>
+                <h1 className='text-5xl font-bold  text-center mb-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent '  >KINGLYWORDS </h1>
                 <div className='flex flex-col gap-2 ' >
                     <div className='cursor-pointer  flex justify-end ' >
-                        <p  onClick={onCopyText}  className="bg-blue-500 px-2 py-2 rounded-md" >copy</p>
+                        <p  onClick={onCopyText}  className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white py-2 px-4 rounded" >copy</p>
                     </div>
 
                     <div className='' >
@@ -58,12 +58,12 @@ loading === true ?
 
 <div className='flex items-center justify-center border border-gray-300 rounded-md p-4 w-full h-72 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500' >
 
-<img src={loading_img} alt="loading " />
+<img  className='w-1/2 '  src={loading_img} alt="loading " />
 
    </div> 
 
 
-: (<textarea ref={copyref}  readOnly value={generatedtext}   className='border border-gray-300 rounded-md p-4 w-full h-72 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500'  placeholder='GENERATED TEXT WILL APPEAR HERE' name="" id=""></textarea>
+: (<textarea ref={copyref}  readOnly value={generatedtext}   className=  ' italic border border-gray-300 rounded-md p-4 w-full h-72 resize-none focus:outline-none focus:ring-2 focus:ring-purple-500'  placeholder='GENERATED TEXT WILL APPEAR HERE' name="" id=""></textarea>
 )
 
 
@@ -81,11 +81,11 @@ loading === true ?
 
 
 <div  className="flex pt-4" >
-    <input value={input}  onChange={(e)=>{setInput(e.target.value)}}  className="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+    <input value={input}  onChange={(e)=>{setInput(e.target.value)}}  className=" italic border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-purple-500"
     
   type="text"   placeholder="Enter your prompt..." />
 
-<button  onClick={genText}   className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+<button  onClick={genText}   className="ml-2 px-4  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white py-2 rounded  hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
              >Generate</button>
 
 
